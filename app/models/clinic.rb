8cb2d83a16455a51b9b8c5fbb3bb85df1723a2a1
Umapathy <umapathymanohar@gmail.com>
2015-10-27 18:16:17 +0530

    Paperclip

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -2,7 +2,14 @@ class Clinic < ActiveRecord::Base
   has_one :user
   has_many :procedures
 
-has_attached_file :asset, photos: {thumb: "100x100#"}
-  	
+
+  	if Rails.env.development?
+		has_attached_file :photos, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.jpg"
+	else
+		has_attached_file :photos, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.jpg",
+						:storage => :dropbox,
+						:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+						:path => ":style/:id_:filename"
+	end
 	validates_attachment_content_type :photos, :content_type => /\Aimage\/.*\Z/
 end
